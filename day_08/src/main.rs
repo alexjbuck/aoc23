08c535e7eddb671a8ca8bd19d5f3a5bc11f805c7
Alexander Buck <abuck@spear.ai>
2023-12-10 19:41:23 -0500

    initial day 08

---
+++ day_08/src/main.rs
@@ -0,0 +1,7 @@
+use day_08::part1;
+use day_08::part2b;
+
+fn main() {
+    part1::run();
+    part2::run();
+}
---
+++ day_08/src/part1.rs
@@ -0,0 +1,8 @@
+pub fn run() {
+    let input = include_str!("input.txt");
+    println!("Part 1: {}", process(input));
+}
+
+fn process(input: &str) -> usize {
+    1
+}
