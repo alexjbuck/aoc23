92eb2a63379dda21e9442b09316d1640146f45cb
alexjbuck <abuck@spear.ai>
2023-12-23 18:27:13 -0500

    Day 09

---
+++ day_09/src/main.rs
@@ -0,0 +1,7 @@
+use day_09::part1;
+use day_09::part2;
+
+fn main() {
+    part1::run();
+    part2::run();
+}
---
+++ day_09/src/part1.rs
@@ -0,0 +1,39 @@
+pub fn run() {
+    let input = include_str!("input.txt");
+    println!("Part 1: {}", process(input));
+}
+
+fn process(input: &str) -> isize {
+    input.lines().map(handle_line).sum()
+}
+
+fn handle_line(line: &str) -> isize {
+    let sequence = line.split_whitespace().map(|x| x.parse::<isize>().unwrap()).collect::<Vec<_>>();
+    find_next(sequence)
+}
+
+fn find_next(sequence: Vec<isize>) -> isize {
+    if sequence.iter().all(|x| x==&0) {
+        0
+    } else {
+        sequence.last().unwrap() + find_next(diff(&sequence))
+    }
+}
+
+fn diff(sequence: &Vec<isize>) -> Vec<isize> {
+    sequence.windows(2).map(|x| x[1]-x[0]).collect::<Vec<_>>()
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let input = "0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45";
+        assert_eq!(process(input), 114)
+    }
+}
