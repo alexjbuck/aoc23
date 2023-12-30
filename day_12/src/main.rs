c2c62a7bc22aeba6b7f90f524ee6cafe6e149253
alexjbuck <abuck@spear.ai>
2023-12-30 15:06:21 -0500

    fix

--- day_12/src/main.rs
+++ day_12/src/main.rs
@@ -1,6 +1,6 @@
 use day_12::{part1,part2};
 
 fn main() {
-    println!("{}",part1::part_1(include_str!("input.txt")));
+    part1::run();
     println!("{}",part2::part_2(include_str!("input.txt")));
 }
